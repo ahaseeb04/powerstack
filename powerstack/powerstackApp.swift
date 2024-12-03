7ef6a00d72e1504967fb3bbc2e2a66f425d898cb
Abdul Haseeb <ahaseeb408@gmail.com>
2024-12-03 17:28:46 -0500

    Initial Commit

---
+++ powerstack/powerstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  powerstackApp.swift
+//  powerstack
+//
+//  Created by Abdul Haseeb on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct powerstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ powerstackTests/powerstackTests.swift
@@ -0,0 +1,17 @@
+//
+//  powerstackTests.swift
+//  powerstackTests
+//
+//  Created by Abdul Haseeb on 2024-12-01.
+//
+
+import Testing
+@testable import powerstack
+
+struct powerstackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
