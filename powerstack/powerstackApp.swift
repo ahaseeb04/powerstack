73efc2323c6f74ae9e162cfe7466b66ad940aafa
Abdul Haseeb <ahaseeb408@gmail.com>
2024-12-10 23:11:51 -0500

    Restrict app orientation to portrait mode

--- powerstack/powerstackApp.swift
+++ powerstack/powerstackApp.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 @main
 struct powerstackApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
     }
 }
 
