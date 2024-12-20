fc8026a87ee85290ee0bf7a0a755c479c4bec498
Abdul Haseeb <ahaseeb408@gmail.com>
2024-12-20 01:48:52 -0500

    Added conditional check

--- powerstack/Views/OpenPowerliftingSearchView.swift
+++ powerstack/Views/OpenPowerliftingSearchView.swift
@@ -201,7 +201,7 @@ struct OpenPowerliftingSearchView: View {
     
     private var lifterProgressView: some View {
         Group {
-            if let lifter = viewModel.lifters.first {
+            if let lifter = viewModel.lifters.first, lifter.progress.total > 0 {
                 VStack(spacing: 10) {
                     Text("Progress")
                         .font(.system(size: 24))
